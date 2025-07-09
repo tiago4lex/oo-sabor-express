db50c228f9143ce6a8d34b612a227c8c7c32b65a
Tiago <tiago.4lexandre@gmail.com>
2025-07-08 22:22:02 -0300

    Refactor listar_restaurantes method to use class reference and improve state management

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,17 +10,19 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f"{'Nome do Restaurante'.ljust(25)} | {'Categoria do Restaurante'.ljust(25)} | {'Status'}")
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '✔' if self._ativo else '✘'
     
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
 restaurante_praca = Restaurante('Cantina da Praça', 'Gourmet')
-restaurante_praca._ativo = True
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express','Italiano')
-
-Restaurante.listar_restaurantes()
